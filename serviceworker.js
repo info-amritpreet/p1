3adea62885735035368d726ef831f78153891577
info-amritpreet <info.amritpreet@gmail.com>
2025-11-28 22:01:47 +0530

    pwa

---
+++ serviceworker.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/serviceworker.js')
+      .then(registration => {
+        console.log('ServiceWorker registered:', registration);
+      })
+      .catch(error => {
+        console.error('ServiceWorker registration failed:', error);
+      });
+  });
+}
